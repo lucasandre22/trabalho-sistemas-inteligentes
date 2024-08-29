173b0ee6778aace87843f18ba609da5aa5b4086a
lucasandre22 <lucas.andre22@yahoo.com>
2024-08-29 18:06:41 -0300

    teste cego

--- rede-neural/teste_cego.py
+++ rede-neural/teste_cego.py
@@ -4,7 +4,7 @@ import numpy as np
 
 classifier = load_model("./modelo_rede_neural_classificador.h5")
 
-def read_vital_signals(self, file_path):
+def read_vital_signals(file_path):
     sinais_virais = {}
     dir_path = os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(dir_path, file_path), 'r') as file:
@@ -15,9 +15,12 @@ def read_vital_signals(self, file_path):
             sinais_virais[id_vitima] = sinais
     return sinais_virais
 
-filename = '../datasets/data_4000v/env_vital_signals.txt'
-sinais_vitais = read_vital_signals(filename)
+file_path = '../datasets/data_408v_94x94/env_vital_signals_cego.txt'
+sinais_vitais = read_vital_signals(file_path)
 last_three_signals = sinais_vitais[-3:]
 last_three_signals_array = np.array(last_three_signals).reshape(1, -1)
 #Predict severity value using the regressor
 severity_value = classifier.predict(last_three_signals_array)[0][0]
+
+#gerar arquivo pred.txt
+#id, x, y, gravity, class
